6120dad12146be2a5789f608e375cb2f76ad9323
everett-hayes <everetty2k@gmail.com>
2021-02-11 22:13:33 -0600

    Fixing typos in CRUD actions

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -8,20 +8,20 @@ class Api::V1::PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
 
-        render json: @posts, status: 200
+        render json: @post, status: 200
     end
 
     def create
         @post = Post.create(post_params)
 
-        render json: @posts, status: 200
+        render json: @post, status: 200
     end
 
     def update
         @post = Post.find(params[:id])
         @post.update(post_params)
 
-        render json: @posts, status: 200
+        render json: @post, status: 200
     end
 
     def destroy
