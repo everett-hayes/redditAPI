9944f60a1dbe6198b4dc26ecb16c7588b86bd74d
everett-hayes <everetty2k@gmail.com>
2021-03-10 17:19:41 -0600

    adding a new field to the model

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_02_10_235451) do
+ActiveRecord::Schema.define(version: 2021_03_10_231359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +7,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_235451) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vote"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 Post.create([
-    { content: 'this is the first reddit post in history'},
-    { content: 'this content was loaded from our API'},
-    { content:  'have you fellow intellectuals considered GME?'},
-    { content: 'everett\'s new reddit is much better than the old one'},
-    { content: 'Elon musk is our lord and savior'}
+    { content: 'this is the first reddit post in history', vote: 1},
+    { content: 'this content was loaded from our API', vote: 2},
+    { content:  'have you fellow intellectuals considered GME?', vote: 3},
+    { content: 'everett\'s new reddit is much better than the old one', vote: 4},
+    { content: 'Elon musk is our lord and savior', vote: 5}
 ])
